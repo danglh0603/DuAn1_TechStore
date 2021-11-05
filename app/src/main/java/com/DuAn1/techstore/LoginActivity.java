ac0723922c6533de6d86432b8e800fccad7f6dd0
Lê Hải Đăng <lehaidangzdin123@gmail.com>
2021-11-05 21:40:37 +0700

    update giao diện

--- app/src/main/java/com/DuAn1/techstore/LoginActivity.java
+++ app/src/main/java/com/DuAn1/techstore/LoginActivity.java
@@ -1,31 +1,34 @@
 package com.DuAn1.techstore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class LoginActivity extends AppCompatActivity {
-    EditText edUserName,edPassword;
-    Button btnLogin;
+    private EditText edUsername;
+    private EditText edPassword;
+    private Button btnLogin;
+    private Button btnRegister;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setTitle("Dang Nhap");
-        edUserName = findViewById(R.id.edUsername);
+        edUsername = findViewById(R.id.edUsername);
         edPassword = findViewById(R.id.edPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
 
-        SharedPreferences pref = getSharedPreferences("USER_FILE",MODE_PRIVATE);
-        String user = pref.getString("USERNAME","");
-        String pass = pref.getString("PASSWORD","");
-
-
-        edUserName .setText(user);
-        edPassword.setText(pass);
+//        SharedPreferences pref = getSharedPreferences("USER_FILE", MODE_PRIVATE);
+//        String user = pref.getString("USERNAME", "");
+//        String pass = pref.getString("PASSWORD", "");
+//
+//
+//        edUserName.setText(user);
+//        edPassword.setText(pass);
 
 
     }
--- app/src/main/java/com/DuAn1/techstore/SplashScreenActivity.java
+++ app/src/main/java/com/DuAn1/techstore/SplashScreenActivity.java
@@ -1,10 +1,10 @@
 package com.DuAn1.techstore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,13 +12,10 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
     }
 }
