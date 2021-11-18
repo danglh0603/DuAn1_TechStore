2ba8c74bdf27e39e442f33fb87341fe98cfe2587
84342 <hoangntph15623>
2021-11-18 21:44:37 +0700

    hí ae

---
+++ app/src/main/java/com/DuAn1/techstore/dangki.java
@@ -0,0 +1,13 @@
+package com.DuAn1.techstore;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dangki  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dangki);
+    }
+}
