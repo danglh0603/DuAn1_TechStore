f58d2e8eb68f43d9d34d060821f446ccbd1f5dd3
NguyenKhacTuan <@tts.tuannk>
2021-12-02 13:31:01 +0700

    update feture change user

--- app/src/main/java/com/DuAn1/techstore/DAO/Server.java
+++ app/src/main/java/com/DuAn1/techstore/DAO/Server.java
@@ -4,7 +4,7 @@ public class Server {
     public static String localhost = "https://duan1techstore.000webhostapp.com";
     public static String dangNhap = localhost + "/api/login.php";
     public static String dangKy = localhost + "/api/register.php";
-    public static String getAllSP = localhost + "/api/getAll_SP.php";
+    public static String getAllSP = localhost + "/api/SPMoiNhat.php";
     public static String yeuThich = localhost + "/api/yeuThich.php";
     public static String sanPhamTheoTL = localhost + "/api/getSpTheoTL.php";
     public static String getKhachHang = localhost + "/api/getKhachHang.php";
